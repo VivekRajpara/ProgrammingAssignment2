745d6719931b1213927bebb339e2e46b6a67e0f8
VivekRajpara <vivekrajpara12292@gmail.com>
2016-01-25 10:57:03 +0530

    Updated solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function - makeCacheMatrix returns list of functions for set and get matrix and its inverse
+## The function - cacheSolve takes the list from the first function and calculates and sets its inverse.  
+##  If the inverse is already set, the cached value is used
 
-## Write a short comment describing this function
+
+## makeCacheMatrix will create a matrix x, and returns functinos for set,get x and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverseMatrixCached <- NULL 
+  
+  set <- function(inputValue = matrix()) {
+    x <<- inputValue
+    inverseMatrixCached <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(newInverse) {
+    inverseMatrixCached <<- newInverse 
+    return(inverseMatrixCached)
+  }
+  
+  getInverse  <- function() inverseMatrixCached
+
+  list(set=set, get=get,
+       setInverse=setInverse, 
+       getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## With list returned from first function, this function will first check to see
+## if there's already a cached inverse, return it otherwise will solve its inverse and set/return it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+  
+  ## if there is cached inverse
+  inversed <- x$getInverse() 
+  
+  ##check if there's a cached value AND it's a matrix
+  if(!is.null(inversed) && is.matrix(inversed)) { 
+    message("Inverse from cached data")
+    return(inversed)
+  }
+  
+  ## otherwise get the matrix
+  changedMatrix <- x$get()  
+  
+  ## try to solve the matrix and catch errors and warnings
+  inversed <- tryCatch({ 
+    solve(changedMatrix)
+  }, warning=function(w) {
+    message("result may not be proper")
+    message(w)
+  }, error=function(e) {
+    message("error while solving matrix")
+    message(e)
+  })
+  
+  ## set new inverse
+  message("Setting the value of inverse to:") 
+  x$setInverse(inversed)
 }
+
